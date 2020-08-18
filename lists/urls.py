bff7627577c6f459e5d78634cc3f9fe1510c8b73
abstractlyZach <zach3lee@gmail.com>
2020-08-17 18:46:43 -0700

    Start over from the beginning

--- lists/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from lists import views
-
-urlpatterns = [
-	url(r'^new$', views.new_list, name='new_list'),
-        url(r'^(\d+)/$', views.view_list, name='view_list'),
-]

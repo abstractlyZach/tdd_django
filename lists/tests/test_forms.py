4407bd75dbca94ce75abb4dff7ee59b5e110e356
abstractlyZach <zach3lee@gmail.com>
2020-12-19 09:24:04 -0800

    chapter 14 (#26)
    * add new form for list items
    
    * Use new form in home_page, simplify tests. NB breaks stuff
    
    * Rename all item input ids and names. still broken
    
    * use form in all views, back to working state
    
    * Use ItemForm to save items to the db

---
+++ lists/tests/test_forms.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from lists.forms import EMPTY_ITEM_ERROR
+from lists.forms import ItemForm
+from lists.models import List
+from lists.models import Item
+
+
+class TestForms(TestCase):
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        assert 'placeholder="Enter a to-do item"' in form.as_p()
+        assert 'class="form-control input-lg"' in form.as_p()
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={"text": ""})
+        assert not form.is_valid()
+        assert form.errors["text"] == [EMPTY_ITEM_ERROR]
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={"text": "do me"})
+        new_item = form.save(for_list=list_)
+        assert new_item == Item.objects.first()
+        assert new_item.text == "do me"
+        assert new_item.list == list_

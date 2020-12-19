67539f0bd9c70709f907df95c418e9dc861dc572
abstractlyZach <zach3lee@gmail.com>
2020-12-18 21:43:02 -0800

    Add linting (#24)
    * Run black on the whole project
    
    * Add pre-commit config file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -3,12 +3,12 @@ import random
 from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 
-REPO_URL = 'https://github.com/abstractlyzach/tdd_django.git'
+REPO_URL = "https://github.com/abstractlyzach/tdd_django.git"
 
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-    source_folder = site_folder + '/source'
+    site_folder = f"/home/{env.user}/sites/{env.host}"
+    source_folder = site_folder + "/source"
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
     _update_settings(source_folder, env.host)
@@ -18,46 +18,39 @@ def deploy():
 
 
 def _create_directory_structure_if_necessary(site_folder):
-    for subfolder in ('database', 'static', 'source'):
-        run(f'mkdir -p {site_folder}/{subfolder}')
+    for subfolder in ("database", "static", "source"):
+        run(f"mkdir -p {site_folder}/{subfolder}")
 
 
 def _get_latest_source(source_folder):
-    if exists(source_folder + '/.git'):
-        run(f'cd {source_folder} && git fetch')
+    if exists(source_folder + "/.git"):
+        run(f"cd {source_folder} && git fetch")
     else:
-        run(f'git clone {REPO_URL} {source_folder}')
+        run(f"git clone {REPO_URL} {source_folder}")
     current_commit = local("git log -n 1 --format=%H", capture=True)
-    run(f'cd {source_folder} && git reset --hard {current_commit}')
+    run(f"cd {source_folder} && git reset --hard {current_commit}")
 
 
 def _update_settings(source_folder, site_name):
-    settings_path = source_folder + '/superlists/settings.py'
+    settings_path = source_folder + "/superlists/settings.py"
     sed(settings_path, "DEBUG = True", "DEBUG = False")
-    sed(settings_path, 'ALLOWED_HOSTS =.+$',
-        f'ALLOWED_HOSTS = ["{site_name}"]')
+    sed(settings_path, "ALLOWED_HOSTS =.+$", f'ALLOWED_HOSTS = ["{site_name}"]')
 
-    secret_key_file = source_folder + '/superlists/secret_key.py'
+    secret_key_file = source_folder + "/superlists/secret_key.py"
     if not exists(secret_key_file):
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
+        key = "".join(random.SystemRandom().choice(chars) for _ in range(50))
         append(secret_key_file, f'SECRET_KEY = "{key}"')
-    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+    append(settings_path, "\nfrom .secret_key import SECRET_KEY")
 
 
 def _poetry_install(source_folder):
-    run(f'cd {source_folder} && poetry install')
+    run(f"cd {source_folder} && poetry install")
 
 
 def _update_static_files(source_folder):
-    run(
-        f'cd {source_folder}'
-        ' && poetry run python manage.py collectstatic --noinput'
-    )
+    run(f"cd {source_folder}" " && poetry run python manage.py collectstatic --noinput")
 
 
 def _update_database(source_folder):
-    run(
-        f'cd {source_folder}'
-        ' && poetry run python manage.py migrate --noinput'
-    )
+    run(f"cd {source_folder}" " && poetry run python manage.py migrate --noinput")
